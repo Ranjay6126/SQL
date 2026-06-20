a4e385b51cf0774f7b124d5db0bea3e0426a5527
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-20 23:34:15 +0530

    same salary

--- Interview Question/same salary.sql
+++ Interview Question/same salary.sql
@@ -4,3 +4,14 @@ FROM employees e1
 JOIN employees e2
 ON e1.salary = e2.salary
 AND e1.id <> e2.id;
+
+-- 13. Find Total Salary Department Wise
+SELECT department_id, SUM(salary) AS total_salary
+FROM employees
+GROUP BY department_id;
+
+
+-- 14. Find Employees Whose Name Starts with 'A'
+SELECT *
+FROM employees
+WHERE name LIKE 'A%';
